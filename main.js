e7b751da6fbd3045a0c3da5b852390dccb07067a
binohub <yeebino001@gmail.com>
2022-06-29 19:05:05 +0200

    start

---
+++ main.js
@@ -0,0 +1,55 @@
+const images = [
+    {
+        url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+        title: 'Svezia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Perù',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+        title: 'Chile',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Argentina',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+        title: 'Colombia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+];
+
+let imageActive = 0;
+const parent = document.querySelector(".carosello-container-img")
+images.forEach((image, index)=>{
+
+    console.log(image.url, image.title, image.description);
+    const newSlide = document.createElement('div');
+    newSlide.classList.add('d-none');
+    if (imageActive === index){
+        newSlide.classList.remove("d-none");
+        newSlide.classList.add("active");
+    }
+    const newImg = document.createElement('img');
+    newImg.src =image.url;
+    const newTitle = document.createElement('h2');
+    newTitle.innerHTML = image.title;
+    const newDescription = document.createElement('p');
+    newDescription.innerHTML = image.description;
+
+    newSlide.append(newImg);
+    newSlide.append(newTitle);
+    newSlide.append(newDescription);
+
+    parent.append(newSlide);
+
+})
